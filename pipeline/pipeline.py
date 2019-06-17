480ee746095f8e17ea561e185a22bd24c50e5166
Kevin Vecmanis <vanaurum@vanurum-master.local>
2019-06-17 17:19:01 -0400

    ..

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -25,7 +25,8 @@ def run(argv=None):
                         help='Output file to write results to.')
 
     known_args, pipeline_args = parser.parse_known_args(argv)
-
+    print(known_args)
+    print(pipeline_args)
     # We use the save_main_session option because one or more DoFn's in this
     # workflow rely on global context (e.g., a module imported at module level).
     pipeline_options = PipelineOptions(pipeline_args)
