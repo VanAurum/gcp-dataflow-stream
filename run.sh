9dc68f1d6c86f2e251666110a44d3bcfb8465d9d
Kevin Vecmanis <vanaurum@vanurum-master.local>
2019-06-17 17:24:58 -0400

    ..

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ python -m pipeline.pipeline \
   --runner DataflowRunner \
   --staging_location gs://vanaurum-stock-stream/staging \
   --temp_location gs://vanaurum-stock-stream/temp \
-  
 --input_mode stream \
 --input_topic stock-stream \
 --output gs://vanaurum-stock-stream
